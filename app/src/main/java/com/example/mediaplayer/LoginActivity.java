4db3281af13150e076bca81f16bfa733fc13b86a
b0ink <40929320+b0ink@users.noreply.github.com>
2024-04-24 16:27:48 +1000

    Add signup logic

--- app/src/main/java/com/example/mediaplayer/LoginActivity.java
+++ app/src/main/java/com/example/mediaplayer/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.mediaplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USERNAME = "extra_username";
+
+    public EditText etUsername;
+    public EditText etPassword;
+    public Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,18 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etUsername = findViewById(R.id.editTextUsername);
+        etPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        Intent intent = getIntent();
+        if(intent != null && intent.hasExtra(EXTRA_USERNAME)){
+            String username = intent.getStringExtra(EXTRA_USERNAME);
+            if(username != null && !username.isEmpty()){
+                etUsername.setText(username);
+                Toast.makeText(this, "Successfully create account, you may now login", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mediaplayer/SignupActivity.java
+++ app/src/main/java/com/example/mediaplayer/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.example.mediaplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
+
+
+    public EditText etUsername;
+    public EditText etFullname;
+    public EditText etPassword;
+    public EditText etConfirmPassword;
+
+    public Button btnCreateAccount;
+
+    public DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,63 @@ public class SignupActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        db = DatabaseHelper.getInstance(this);
+
+        etUsername = findViewById(R.id.editTextUsername);
+        etFullname = findViewById(R.id.editTextFullname);
+        etPassword = findViewById(R.id.editTextPassword);
+        etConfirmPassword = findViewById(R.id.editTextPasswordConfirm);
+        btnCreateAccount = findViewById(R.id.buttonCreateAccount);
+
+        btnCreateAccount.setOnClickListener(view -> {
+
+            String fullname = etFullname.getText().toString();
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+            String passwordConfirm = etConfirmPassword.getText().toString();
+
+            if (username.isEmpty() || fullname.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()) {
+                Toast.makeText(this, "Fields can not be left blank", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (fullname.length() > 128 || fullname.length() < 3) {
+                Toast.makeText(this, "Full name must be at least 3 characters", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (username.length() > 25 || fullname.length() < 5) {
+                Toast.makeText(this, "Username must between 5-25 characters", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (password.length() > 255 || password.length() < 5) {
+                Toast.makeText(this, "Password must be at least 5 characters", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!password.equals(passwordConfirm)) {
+                Toast.makeText(this, "Passwords do not match, please try again", Toast.LENGTH_SHORT).show();
+                etPassword.setText("");
+                etConfirmPassword.setText("");
+                return;
+            }
+
+            if (db.saveNewUser(fullname, username, password)) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.putExtra(LoginActivity.EXTRA_USERNAME, username.toLowerCase());
+                startActivity(intent);
+                finish();
+                return;
+            }
+
+            Toast.makeText(this, "An error occurred, please try again", Toast.LENGTH_LONG).show();
+
+            etPassword.setText("");
+            etConfirmPassword.setText("");
+
         });
     }
 }
